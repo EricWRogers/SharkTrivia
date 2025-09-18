cd5d9e3aee43949798e441286eeeda998215128c
Crow <crowmiller6@gmail.com>
2025-09-18 16:19:37 -0500

    TMP CANT BEAT ME
    "using TMP;" is real actually and implimenting it makes things work for some reason

--- Assets/Scripts/Journal/ButtonValueRandomizer.cs
+++ Assets/Scripts/Journal/ButtonValueRandomizer.cs
@@ -1,16 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ButtonValueRandomizer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public TMP_Text buttonText;
+    
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
+    
+    
     void Update()
     {
-        
+
     }
+
+    public void NewText()
+    {
+        buttonText.text += " B";
+    }
+
 }
