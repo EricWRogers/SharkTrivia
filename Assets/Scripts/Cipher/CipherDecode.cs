bf199f5bebd528711aa7fcdfbc6e8ec868c87c7f
Schrod <schrosam@gmail.com>
2025-09-25 17:59:27 -0500

    Changed the CipherDecode.cs script to not be nonsensical
    I don't know fully what I was originally cooking, but it should actually do what was intended for it to do now (with code blocks to comment or uncomment depending on what you want it to do)

--- Assets/Scripts/Cipher/CipherDecode.cs
+++ Assets/Scripts/Cipher/CipherDecode.cs
@@ -1,10 +1,6 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEditor.Animations;
-using JetBrains.Annotations;
+using System.Linq;
 
 // //Originally Programmed by Samuel (Scott)
 
@@ -14,7 +10,7 @@ public class CipherDecode : MonoBehaviour
     public List<char> keys = new List<char>();
     public List<char> values = new List<char>();
 
-    public Dictionary<char, char> charAssignments = new Dictionary<char, char>
+    private Dictionary<char, char> charAssignments = new Dictionary<char, char>
     {
         //tilde represents an english character which has not been assigned a ciphertext equivalent 
         {'a', '~'},{'b', '~'},{'c', '~'},{'d', '~'},
@@ -26,16 +22,57 @@ public class CipherDecode : MonoBehaviour
         {'y', '~'},{'z', '~'}
     };
 
-    //KEYS AND VALUES MUST BE THE SAME LENGTH!!!
-    public void CharAssignment()
+    //pass in the key and value you are trying to edit
+    public int CharAssignment(char key, char value)
     {
         for (int i = 97; i < 122; i++)
         {
-            if (charAssignments.ContainsKey((char)i))
+            Debug.Log("Checking character: " + (char)i);
+
+            //This if handles the case where the value that goes to the key in question is blank ('~') and the new value is not already found elsehwere
+            if (charAssignments[(char)i] == '~' && (char)i == key && !charAssignments.ContainsValue(value))
             {
-                
+                charAssignments[key] = value;
             }
+
+            //This if handles the case where the value that goes to the key in question is not blank but the new value also isn't found anywhere else in the journal
+            else if (charAssignments[(char)i] != '~' && (char)i == key && !charAssignments.ContainsValue(value))
+            {
+                //Return an error code (some negative number) if you want this operation to be illegal, elsewise just overwrite the value if you're ok with it
+
+                // return -1;
+
+                //OR
+
+                // charAssignments[key] = value;
+            }
+
+            //This if handles the case where the new value is already used somehwere else in the journal with some subcases
+            else if ((char)i == key && charAssignments.ContainsValue(value))
+            {
+                //Return an error code (some other negative number) if you want this operation to be illegal, elsewise erase where the new value already was and put it here
+
+                //Case where the value that goes to the key in question is blank
+                // if (charAssignments[(char)i] == '~')
+                // {
+                //     // return -2;
+                // }
+
+                //Case where the value that goes to the key in question is blank
+                // if (charAssignments[(char)i] != '~')
+                // {
+                //     // return -3;
+                // }
+
+                //OR
+
+                // var firstKey = charAssignments.FirstOrDefault(kvp => kvp.Value == value).Key;
+                // charAssignments[firstKey] = '~';
+                // charAssignments[key] = value;
+            }
+
         }
+        return 0;
 
     }
 }
