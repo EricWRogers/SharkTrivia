35e5e9a5a02f2b8eb46814580eb8d2516f21e7bd
SilverSonic21 <Silversonic21beast@gmail.com>
2025-09-25 16:40:32 -0500

    Enemy Spawner

--- Assets/Scripts/Minigames/SharkShootout/Enemy.cs
+++ Assets/Scripts/Minigames/SharkShootout/Enemy.cs
@@ -2,24 +2,25 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEngine.Rendering.Universal;
 
 
 public class Enemy : MonoBehaviour
 
 {
 
-    private Rigidbody2D rb;
+    public float Life = 3;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-      // rb.drag = 10;   
+       
     }
 }
