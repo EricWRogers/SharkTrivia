8582aaeb000a630e216a20936746c574b5e6ab98
K.D.W <kadendw23@gmail.com>
2025-09-30 16:20:14 -0500

    Lose Screen + Pause Menu (Temple Shark)
    - Added lose screen
    - Added pause menu
    - Edited Game Over Manager (GameOverShow)
    - GameOverShow now shows lose screen
    - Added an Event (Can be used to set obj's inactive/active)

--- Assets/Scripts/Levels/Minigames/GeneralUse/GameOverManager.cs
+++ Assets/Scripts/Levels/Minigames/GeneralUse/GameOverManager.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 
 public class GameOverManager : MonoBehaviour
 {
-    public GameObject ball;
-    public GameObject pins;
-    public GameObject lane;
-    
+    public UnityEvent ActiveSwitch;
+    public GameObject LoseScreen;
+
     public void GameOverShow()
     {
-        SceneManager.LoadScene("BackStage");
-        ball.SetActive(false);
-        pins.SetActive(false);
-        lane.SetActive(false);
+        LoseScreen.SetActive(true);
+        ActiveState();
+    }
+
+    public void ActiveState()
+    {
+        ActiveSwitch.Invoke();
     }
     public void Restart()
     {
@@ -25,6 +28,11 @@ public class GameOverManager : MonoBehaviour
     {
         Time.timeScale = 1f; 
         SceneManager.LoadScene("MainMenu"); // Loads the main menu
+    }
+    public void BackStage()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("BackStage"); // Loads the backstage
     }
     public void Quit()
     {
