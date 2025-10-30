5f33c4598700e6111d100621c5e59e86f200a532
K.D.W <kadendw23@gmail.com>
2025-10-30 17:42:43 -0500

    Temple Shark Testing 2
    - Randomization of segments now ignore the final segment until a max amount of  random segments have spawned.
    
    - Created a new segment Level.
    
    - Final segment will need reworking (winning: hard)

--- Assets/Scripts/Levels/Minigames/Temple Shark/SegmentGenerator.cs
+++ Assets/Scripts/Levels/Minigames/Temple Shark/SegmentGenerator.cs
@@ -5,16 +5,18 @@ using System.Collections.Generic;
 public class SegmentGenerator : MonoBehaviour
 {
     [Header("Generator")]
-    public GameObject[] segment;
+    public GameObject[] segments; // Array of segment prefabs
+    public int maxSegments = 10; // Total segments before placing the final one
 
     [SerializeField] int zPos = 50;
     [SerializeField] float waitTime = 5f;
-    [SerializeField] bool segmentCreate = false;
-    [SerializeField] int segmentNum; 
+
+    private bool segmentCreate = false;
+    public int generatedCount = 0; // Tracks how many segments have been spawned
 
     void Update()
     {
-        if (segmentCreate == false)
+        if (!segmentCreate && generatedCount < maxSegments + 1)
         {
             segmentCreate = true;
             StartCoroutine(SegmentGen());
@@ -23,9 +25,20 @@ public class SegmentGenerator : MonoBehaviour
 
     IEnumerator SegmentGen()
     {
-        segmentNum = Random.Range(0, 3);
-        Instantiate(segment[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        // If we haven't reached max segments
+        if (generatedCount < maxSegments)
+        {
+            int segmentNum = Random.Range(0, segments.Length - 1); // Excludes last segment (index 3)
+            Instantiate(segments[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        }
+        else
+        {
+            // Spawn the final (4th, index 3) segment
+            Instantiate(segments[3], new Vector3(0, 0, zPos), Quaternion.identity);
+        }
+        generatedCount++;
         zPos += 50;
+
         yield return new WaitForSeconds(waitTime);
         segmentCreate = false;
     }
