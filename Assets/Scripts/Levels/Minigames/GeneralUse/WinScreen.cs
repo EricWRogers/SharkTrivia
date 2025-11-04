e58ad32a464678156814f4b3be202a438d49b289
ChaneyWilliams <chaneyw2004@gmail.com>
2025-11-04 16:57:36 -0600

    Fixed winScreen timeScale
    LevelManager is an IEnum so it needs time scale to not be 0
    timeScale was getting overwritten in Update

--- Assets/Scripts/Levels/Minigames/GeneralUse/WinScreen.cs
+++ Assets/Scripts/Levels/Minigames/GeneralUse/WinScreen.cs
@@ -19,20 +19,17 @@ public class WinScreen : MonoBehaviour
     public TMP_Text winTime;
     public TMP_Text winStat;
 
-    void Start(){
-        HideWinScreen();
-
-    }
-
-    public void DisplayWinResults(int score)
-    {    //whenever a minigame is over, call function
-        int points = score;
+    public void DisplayWinResults()
+    {
+        //whenever a minigame is over, call function
+        StopGame();
+        int points = ScoreManager.instance.score;
 
 
         if (points >= winThreshold)
         {
             winText.text = "YOU WIN!";
-            TotalScore.instance.AddPoints(score);
+            TotalScore.instance.AddPoints(points);
         }
         if (points < winThreshold)
         {
@@ -45,15 +42,6 @@ public class WinScreen : MonoBehaviour
         winScreen.SetActive(true);
         
         ShowWinScreen();    //show the win screen when the minigame is over
-
-        if (SceneManager.GetActiveScene().name == "SharkShootout")
-        {
-            StopGame();
-        }
-        if (SceneManager.GetActiveScene().name == "Bowling")
-        {
-            StopGame();
-        }
 
     }
 
@@ -68,20 +56,11 @@ public class WinScreen : MonoBehaviour
         //winScreen.SetActive(true);
         ShowWinScreen();    //show the win screen when the minigame is over
 
-        if (SceneManager.GetActiveScene().name == "SharkShootout")
-        {
-            StopGame();
-        }
-        if (SceneManager.GetActiveScene().name == "Bowling")
-        {
-            StopGame();
-        }
-
     }
 
     public void ReturnButton()
     {
-        Debug.Log("CLICK");
+        LevelManager.StaticResume();
         LevelManager.LoadBackStage();   //temp for now
 
         //send player back to what ever scene they entered from.
@@ -98,6 +77,7 @@ public class WinScreen : MonoBehaviour
 
     public void StopGame()
     {
+        //Debug.Log(SceneManager.GetActiveScene().name);
         Time.timeScale = 0;
     }
 }
