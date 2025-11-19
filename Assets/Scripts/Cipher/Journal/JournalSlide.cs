faf3eb15be70752da7f691a1d597495470580f94
Schrod <schrosam@gmail.com>
2025-11-18 21:49:38 -0600

    Added Journal Slide anim (only to Trivia Round 1)

--- Assets/Scripts/Cipher/Journal/JournalSlide.cs
+++ Assets/Scripts/Cipher/Journal/JournalSlide.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class JournalSlide : MonoBehaviour
@@ -9,25 +10,39 @@ public class JournalSlide : MonoBehaviour
     public float smoothTime = 0.3f;
     public AudioSource journalPaper;     // Assign in Inspector
     private bool isDown = true;
+    private GameObject journal;
+    public float closeEnoughVal = 0.1f;
+
+    void Start()
+    {
+        journal = GameObject.Find("JournalUIPrefab");
+        originalPosition = transform.position;
+    }
 
     void FixedUpdate()
     {
-
         if (buttonPressed)
         {
             Vector3 target = isDown ? targetTransform.position : originalPosition;
             transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
 
-            if(Vector2.Distance(transform.position, target) < 0.01){
+            if(Vector2.Distance(transform.position, target) < closeEnoughVal)
+            {
                 buttonPressed = false;
+                isDown = !isDown;
 
+                if(isDown)
+                    journal.transform.GetChild(2).gameObject.SetActive(true);
+                else
+                    journal.transform.GetChild(1).GetChild(0).GetChild(2).gameObject.SetActive(true);
             }
         }
     }
 
-    void JournalOpenClose()
+    public void JournalOpen()
     {
-        //make button not interactable
+        GetComponent<AudioSource>().pitch = 1f + Random.Range(-0.1f, 0.2f);
+        GetComponent<AudioSource>().Play();
         buttonPressed = true;
     }
 }
