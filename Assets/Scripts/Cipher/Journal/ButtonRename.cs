94a7c77e03db90f98dd4d1fc9eeb100debbc5354
Crow <crowmiller6@gmail.com>
2025-11-23 17:51:41 -0600

    TR1 fix

--- Assets/Scripts/Cipher/Journal/ButtonRename.cs
+++ Assets/Scripts/Cipher/Journal/ButtonRename.cs
@@ -24,13 +24,39 @@ public class ButtonRename : MonoBehaviour
 
     public void Close(string _c)
     {
-        returnText.text = _c;
 
-        if (CipherDecode.instance == null)
-            Debug.Log("Cipher is null");
+        if(_c[0] != '~'){
 
-        
-        CipherDecode.instance.CharAssignment(_c[0], newKeyValue);
+            if(CipherDecode.instance.charAssignments[_c[0]] == '~' && CipherDecode.instance.confirmedCharAssignments[_c[0]] == '~')
+                returnText.text = _c;
+
+            if (CipherDecode.instance == null)
+                Debug.Log("Cipher is null");
+
+            
+            CipherDecode.instance.CharAssignment(_c[0], newKeyValue);
+
+
+            //THIS IS INCREDIBLY SCUFFED ALSO, YOU KNOW THE DRILL
+            if((CipherDecode.instance.isCountingGuesses || CipherDecode.instance.isLimitingCorrectness) && (CipherDecode.instance.numGuesses == CipherDecode.instance.maxGuesses || CipherDecode.instance.numCorrectGuesses == CipherDecode.instance.maxCorrectGuesses))
+            {
+                var buttonScripts = FindObjectsByType<GuessLetterMessager>(FindObjectsSortMode.None);
+
+                for(int i = 0; i < buttonScripts.Length; i++)
+                {
+                    buttonScripts[i].OnEnable();
+                }
+            }
+        }
+        else
+            ClearLetter(_c);
+    }
+
+    public void ClearLetter(string _u)
+    {
+        returnText.text = _u;
+
+        CipherDecode.instance.CharAssignment(_u[0], newKeyValue);
 
 
         //THIS IS INCREDIBLY SCUFFED ALSO, YOU KNOW THE DRILL
